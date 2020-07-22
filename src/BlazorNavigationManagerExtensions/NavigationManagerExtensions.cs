e3d75643e16c396e69a40246d520df8f390de23b
Bruno Barrette <bbarrette@pyxis-tech.com>
2020-07-21 23:26:12 -0400

    Initial version of the package with an extension to pass query string parameters

---
+++ src/BlazorNavigationManagerExtensions/NavigationManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+
+namespace BlazorNavigationManagerExtensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static void NavigateTo(this NavigationManager navManager, string uri, IDictionary<string, string> queryString, bool forceLoad = false)
+        {
+            navManager.NavigateTo(QueryHelpers.AddQueryString(uri, queryString), forceLoad);
+        }
+    }
+}
