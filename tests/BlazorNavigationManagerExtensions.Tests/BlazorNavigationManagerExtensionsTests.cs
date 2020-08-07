5f8fe78e3b77ab7ed4a6f69e4cec833aed8270f4
Bruno Barrette <bbarrette@pyxis-tech.com>
2020-08-06 21:49:07 -0400

    Added the test project and tested the NavigateTo method with query params

---
+++ tests/BlazorNavigationManagerExtensions.Tests/BlazorNavigationManagerExtensionsTests.cs
@@ -0,0 +1,53 @@
+using BlazorNavigationManagerExtensions.Fixtures;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BlazorNavigationManagerExtensions
+{
+    public class BlazorNavigationManagerExtensionsTests : IClassFixture<TestNavigationManagerFixture>
+    {
+        private readonly string uri;
+
+        private readonly TestNavigationManager navigationManager;
+
+        public BlazorNavigationManagerExtensionsTests(TestNavigationManagerFixture navigationManagerFixture)
+        {
+            uri = "newPage";
+
+            navigationManager = navigationManagerFixture.NavigationManager;
+        }
+
+        [Fact]
+        public void NavigateTo_QueryParams_ShouldAddASingleQueryParameterToTheUri()
+        {
+            var queryParams = new Dictionary<string, string> { { "param", "value" } };
+            navigationManager.NavigateTo(uri, queryParams);
+
+            navigationManager.NavigatedUri.Should().Be($"{uri}?param=value");
+        }
+
+        [Fact]
+        public void NavigateTo_QueryParams_ShouldAddMultipleQueryParametersToTheUri()
+        {
+            var queryParams = new Dictionary<string, string> {
+                { "first", "firstValue" },
+                { "second", "secondValue" }
+            };
+
+            navigationManager.NavigateTo(uri, queryParams);
+
+            navigationManager.NavigatedUri.Should().Be($"{uri}?first=firstValue&second=secondValue");
+        }
+
+        [Fact]
+        public void NavigateTo_QueryParams_ShouldNavigateEvenIfEmptyQueryParametersWereProvided()
+        {
+            var queryParams = new Dictionary<string, string>();
+
+            navigationManager.NavigateTo(uri, queryParams);
+
+            navigationManager.NavigatedUri.Should().Be(uri);
+        }
+    }
+}
